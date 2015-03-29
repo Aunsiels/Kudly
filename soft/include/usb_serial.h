4f6cac11ea423aef99451402d71fdaf0b15a66bf
Julien Romero <romerojulien34@gmail.com>
2015-03-29 19:43:50 +0200

    doxygen in usb serial file

--- soft/include/usb_serial.h
+++ soft/include/usb_serial.h
@@ -1,19 +1,47 @@
+/**
+ * \file usb_serial.h
+ * \brief Serial over usb connexion
+ * \author KudlyProject
+ * \version 0.1
+ * \date 29/03/2015
+ *
+ * Manages the serial over usb connection.
+ *
+ */
+
 #ifndef USB_SERIAL_H
 #define USB_SERIAL_H
 
-//The serial driver of the usb
-extern SerialUSBDriver SDU2;
-
-//Initialize the serial communication
+/**
+ *
+ * \brief Initializes the usb over serial communication
+ *
+ */
 void init_usb_serial (void);
 
-//Thread that receives the datas
-extern Thread * USBThread;
+/**
+ *
+ * \brief Writes datas over the usb.
+ * \param buffer A buffer containing the datas to send.
+ * \param length The length of the buffer.
+ *
+ * This function writes datas over the usb connection.
+ *
+ */
 
-//Working area of the usb
-extern WORKING_AREA(wa_usb, 128);
+void write_serial(uint8_t * buffer, int size);
 
-//USB Thread function
-msg_t usb_thread (void * p);
+
+/**
+ *
+ * \brief Reads datas over the usb.
+ * \param buffer A buffer where the received datas will be written.
+ * \param length The length of the buffer.
+ *
+ * This function reads datas reveived on the usb.
+ *
+ */
+
+void read_serial(uint8_t * buffer, int size);
 
 #endif
