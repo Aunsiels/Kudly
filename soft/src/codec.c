2c7311b595b1120ac8b7756f07b8028fa85f6f58
Dimitri Tuaz <dimitri.tuaz-torchon@telecom-paristech.fr>
2015-04-01 19:19:51 +0200

    Mode of pins changed for the codec

--- soft/src/codec.c
+++ soft/src/codec.c
@@ -1,8 +1,16 @@
-#include <ch.h>
-#include <hal.h>
+#include "ch.h"
+#include "hal.h"
 
 #include "codec.h"
 
 void codecInit(){
+  /* Change the mode of the pins used for the codec */
+  palSetPadMode(GPIOE,GPIOE_SPI4_XDCS,PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOE,GPIOE_SPI4_XCS,PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOE,GPIOE_CODEC_DREQ,PAL_MODE_INPUT_PULLUP);
+  palSetPadMode(GPIOE,GPIOE_SPI4_SCK,PAL_MODE_ALTERNATE(5));
+  palSetPadMode(GPIOE,GPIOE_SPI4_MISO,PAL_MODE_ALTERNATE(5));
+  palSetPadMode(GPIOE,GPIOE_SPI4_MOSI,PAL_MODE_ALTERNATE(5));
 
+  
 }
