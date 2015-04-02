4c3f106879c2179be9537a48e02a396ab0d60640
Antoine Rousseau <antoine.rousseau9@gmail.com>
2015-04-02 11:06:31 +0200

    Registers definition

--- soft/src/codec.c
+++ soft/src/codec.c
@@ -2,6 +2,25 @@
 #include "hal.h"
 #include "codec.h"
 
+/* Registers definition */
+#define SCI_MODE 0x0
+#define SCI_STATUS 0x1
+#define SCI_BASS 0x2
+#define SCI_CLOCKF 0x3
+#define SCI_DECODE_TIME 0x4
+#define SCI_AUDATA 0x5
+#define SCI_WRAM 0x6
+#define SCI_WRAMADDR 0x7
+#define SCI_HDAT0 0x8
+#define SCI_HDAT1 0x9
+#define SCI_AIADDR 0xA
+#define SCI_VOL 0xB
+#define SCI_AICTRL0 0xC
+#define SCI_AICTRL1 0xD
+#define SCI_AICTRL2 0xE
+#define SCI_AICTRL3 0xF
+
+
 /* Macro used for going in data,command or reset mode */
 #define RESET_MODE palSetPad(GPIOE,GPIOE_SPI4_XDCS);palSetPad(GPIOE,GPIOE_SPI4_XCS)
 #define COMMAND_MODE palSetPad(GPIOE,GPIOE_SPI4_XDCS);palClearPad(GPIOE,GPIOE_SPI4_XCS)
@@ -21,28 +40,28 @@ static uint8_t instruction[4];
 static uint8_t registerContent[4];
 
 static void writeRegister(uint8_t adress, uint16_t command){
-  COMMAND_MODE; 
- 
-  /* Construction of instruction (Write opcode, adress, command) */ 
+  COMMAND_MODE;
+
+  /* Construction of instruction (Write opcode, adress, command) */
   instruction[0] = 0x02;
   instruction[1] = adress;
   instruction[2] = (command >> 8);
   instruction[3] = command;
   spiSend(&SPID4,sizeof(instruction),instruction);
-  
+
   RESET_MODE;
 }
 
 static uint16_t readRegister(uint8_t adress){
   COMMAND_MODE;
-  
-  /* Construction of instruction (Read opcode, adress) */ 
+
+  /* Construction of instruction (Read opcode, adress) */
   instruction[0] = 0x02;
   instruction[1] = adress;
   spiExchange(&SPID4,sizeof(instruction),instruction,registerContent);
-  
+
   RESET_MODE;
-  
+
   /* Return only the 2 last bytes (data from the register) */
   return ((registerContent[2]<<8) + registerContent[3]);
 }
@@ -57,15 +76,15 @@ void codecInit(){
   palSetPadMode(GPIOE,GPIOE_SPI4_MOSI,PAL_MODE_ALTERNATE(5));
 
   RESET_MODE;
-  
+
   /* Start of SPI bus */
   spiAcquireBus(&SPID4);
   spiStart(&SPID4, &hs_spicfg);
   spiSelect(&SPID4);
-   
+
   /* Set Volume */
   writeRegister(0xB,0xF0);
-  
+
   readRegister(0xB);
-  
+
 }
