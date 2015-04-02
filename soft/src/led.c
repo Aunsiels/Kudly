5ba3217025dec361bfc0d9c8672982ed88aed00d
Marc Beillevaire <marc.beillevaire@gmail.com>
2015-04-02 09:48:25 +0200

    Adding HSV function for leds

--- soft/src/led.c
+++ soft/src/led.c
@@ -45,28 +45,45 @@ void ledInit(void) {
     pwmEnableChannel(&PWMD1, 2, 0);
 }
 
-void ledSetColor1(uint8_t r, uint8_t g, uint8_t b) {
-    pwmEnableChannel(&PWMD5, 0, r);
-    pwmEnableChannel(&PWMD5, 1, g);
-    pwmEnableChannel(&PWMD5, 2, b);
+void ledSetColorRGB(int led, int r, int g, int b) {
+    if(led == 1 || led == 0) {
+        pwmEnableChannel(&PWMD5, 0, r);
+        pwmEnableChannel(&PWMD5, 1, g);
+        pwmEnableChannel(&PWMD5, 2, b);
+    } else if(led == 2 || led == 0) {
+        pwmEnableChannel(&PWMD1, 0, r);
+        pwmEnableChannel(&PWMD1, 1, g);
+        pwmEnableChannel(&PWMD1, 2, b);
+    }
 }
 
-void ledSetColor2(uint8_t r, uint8_t g, uint8_t b) {
-    pwmEnableChannel(&PWMD1, 0, r);
-    pwmEnableChannel(&PWMD1, 1, g);
-    pwmEnableChannel(&PWMD1, 2, b);
-}
+void ledSetColorHSV(int led, int h, int s, int v) {
+    int r = 0, g = 0, b = 0;
 
-void ledToggle1(void) {
-    pwmEnableChannel(&PWMD5, 0, 0);
-    pwmEnableChannel(&PWMD5, 1, 0);
-    pwmEnableChannel(&PWMD5, 2, 0);
-}
+    int hueSection = h / 60;
+    int f = (h - hueSection * 60);
 
-void ledToggle2(void) {
-    pwmEnableChannel(&PWMD1, 0, 0);
-    pwmEnableChannel(&PWMD1, 1, 0);
-    pwmEnableChannel(&PWMD1, 2, 0);
+    int val = v * 255 / 100; //Normalized value
+    int l = v * 255 * (100 - s) / 10000;
+    int m = v * 255 / 100 - v * 255 * s * f / 60 / 10000;
+    int n = v * 255 / 100 - v * 255 * s * (60 - f) / 60 / 10000;
+
+    switch(hueSection) {
+        case 0:
+            r = val; g = n; b = l; break;
+        case 1:
+            r = m; g = val; b = l; break;
+        case 2:
+            r = l; g = val; b = n; break;
+        case 3:
+            r = l; g = m; b = val; break;
+        case 4:
+            r = n; g = l; b = val; break;
+        case 5:
+            r = val; g = l; b = m; break;
+    }
+
+    ledSetColorRGB(led, r, g, b);
 }
 
 static msg_t ledTest_thd(void * args) {
@@ -81,31 +98,41 @@ static msg_t ledTest_thd(void * args) {
 
     chThdSleepMilliseconds(500);
 
-    pwmEnableChannel(&PWMD5, 0, 0);
-    pwmEnableChannel(&PWMD5, 1, 0);
-    pwmEnableChannel(&PWMD5, 2, 0);
-
-    pwmEnableChannel(&PWMD1, 0, 0);
-    pwmEnableChannel(&PWMD1, 1, 0);
-    pwmEnableChannel(&PWMD1, 2, 0);
+    ledSetColorRGB(0, 0, 0, 0);
 
     chThdSleepMilliseconds(500);
 
-    setColor_led1(255, 0, 0);
+    ledSetColorRGB(1, 255, 0, 0);
     chThdSleepMilliseconds(400);
-    setColor_led1(0, 255, 0);
+    ledSetColorRGB(1, 0, 255, 0);
     chThdSleepMilliseconds(400);
-    setColor_led1(0, 0, 255);
+    ledSetColorRGB(1, 0, 0, 255);
     chThdSleepMilliseconds(400);
-    setColor_led1(0, 0, 0);
+    ledSetColorRGB(1, 0, 0, 0);
 
-    setColor_led2(255, 0, 0);
+    ledSetColorRGB(2, 255, 0, 0);
     chThdSleepMilliseconds(400);
-    setColor_led2(0, 255, 0);
+    ledSetColorRGB(2, 0, 255, 0);
     chThdSleepMilliseconds(400);
-    setColor_led2(0, 0, 255);
+    ledSetColorRGB(2, 0, 0, 255);
     chThdSleepMilliseconds(400);
-    setColor_led2(0, 0, 0);
+    ledSetColorRGB(2, 0, 0, 0);
+
+    ledSetColorHSV(1, 255, 0, 0);
+    chThdSleepMilliseconds(400);
+    ledSetColorHSV(1, 0, 255, 0);
+    chThdSleepMilliseconds(400);
+    ledSetColorHSV(1, 0, 0, 255);
+    chThdSleepMilliseconds(400);
+    ledSetColorHSV(1, 0, 0, 0);
+
+    ledSetColorHSV(2, 255, 0, 0);
+    chThdSleepMilliseconds(400);
+    ledSetColorHSV(2, 0, 255, 0);
+    chThdSleepMilliseconds(400);
+    ledSetColorHSV(2, 0, 0, 255);
+    chThdSleepMilliseconds(400);
+    ledSetColorHSV(2, 0, 0, 0);
 
     chThdSleep(TIME_INFINITE);
     return 0;
