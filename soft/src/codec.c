c5e55071d99c3b7fc0d7e7e864a345b11f472ca6
Dimitri Tuaz <dimitri.tuaz-torchon@telecom-paristech.fr>
2015-04-02 10:47:27 +0200

    Wrinting of functions for reading and writing in codec registers

--- soft/src/codec.c
+++ soft/src/codec.c
@@ -2,8 +2,10 @@
 #include "hal.h"
 #include "codec.h"
 
-#define COMMAND_MODE palSetPad(GPIOE,GPIOE_SPI4_XDCS);palClearPad(GPIOE,GPIOE_SPI4_XCS);
-#define DATA_MODE    palSetPad(GPIOE,GPIOE_SPI4_XCS);palClearPad(GPIOE,GPIOE_SPI4_XDCS);
+/* Macro used for going in data,command or reset mode */
+#define RESET_MODE palSetPad(GPIOE,GPIOE_SPI4_XDCS);palSetPad(GPIOE,GPIOE_SPI4_XCS)
+#define COMMAND_MODE palSetPad(GPIOE,GPIOE_SPI4_XDCS);palClearPad(GPIOE,GPIOE_SPI4_XCS)
+#define DATA_MODE    palSetPad(GPIOE,GPIOE_SPI4_XCS);palClearPad(GPIOE,GPIOE_SPI4_XDCS)
 
 
 /* SPI configuration (21MHz, CPHA=0, CPOL=0, MSb first) */
@@ -14,9 +16,36 @@ static const SPIConfig hs_spicfg = {
   0
 };
 
-/*  SPI initialisation commands */
-static const uint8_t initbuf[] = {};
+/* Buffer used for construcion of read and write command instructions */
+static uint8_t instruction[4];
+static uint8_t registerContent[4];
 
+static void writeRegister(uint8_t adress, uint16_t command){
+  COMMAND_MODE; 
+ 
+  /* Construction of instruction (Write opcode, adress, command) */ 
+  instruction[0] = 0x02;
+  instruction[1] = adress;
+  instruction[2] = (command >> 8);
+  instruction[3] = command;
+  spiSend(&SPID4,sizeof(instruction),instruction);
+  
+  RESET_MODE;
+}
+
+static uint16_t readRegister(uint8_t adress){
+  COMMAND_MODE;
+  
+  /* Construction of instruction (Read opcode, adress) */ 
+  instruction[0] = 0x02;
+  instruction[1] = adress;
+  spiExchange(&SPID4,sizeof(instruction),instruction,registerContent);
+  
+  RESET_MODE;
+  
+  /* Return only the 2 last bytes (data from the register) */
+  return ((registerContent[2]<<8) + registerContent[3]);
+}
 
 void codecInit(){
   /* Change the mode of the pins used for the codec */
@@ -27,15 +56,16 @@ void codecInit(){
   palSetPadMode(GPIOE,GPIOE_SPI4_MISO,PAL_MODE_ALTERNATE(5));
   palSetPadMode(GPIOE,GPIOE_SPI4_MOSI,PAL_MODE_ALTERNATE(5));
 
+  RESET_MODE;
+  
   /* Start of SPI bus */
   spiAcquireBus(&SPID4);
   spiStart(&SPID4, &hs_spicfg);
   spiSelect(&SPID4);
+   
+  /* Set Volume */
+  writeRegister(0xB,0xF0);
   
-  /* Go in commande mode */
-  COMMAND_MODE
-
-  /* Send initialisation commands */
-  spiSend(&SPID4,sizeof(initbuf),initbuf);
+  readRegister(0xB);
   
 }
