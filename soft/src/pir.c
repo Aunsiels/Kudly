b733ba4b8cf8750225ab2b899ac17dbb95c2caf9
Julien Romero <romerojulien34@gmail.com>
2015-04-02 16:18:00 +0200

    remove ext config in pir

--- soft/src/pir.c
+++ soft/src/pir.c
@@ -9,39 +9,7 @@
 static EXTChannelConfig config[] = {EXT_CH_MODE_BOTH_EDGE 
                                   | EXT_CH_MODE_AUTOSTART
                                   | EXT_MODE_GPIOD, 
-                                  extPir},
-
-/*
- * Configuration of the EXT
- * TODO : when others ext sources, init in an other file
- */
-static EXTConfig extcfg = {
-  {
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL},
-    {EXT_CH_MODE_DISABLED, NULL}
-  }
-};
+                                  extPir};
 
 void pirInit(){
     palSetPadMode(GPIOD,14,PAL_MODE_INPUT_PULLDOWN);
