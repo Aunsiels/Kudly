fd88891bf3464a8877977ebf034b866f9a104e7c
Julien Romero <romerojulien34@gmail.com>
2015-04-04 15:46:38 +0200

    sccb files

---
+++ soft/include/sccb.h
@@ -0,0 +1,14 @@
+/**
+ * \file sccb.h
+ * \brief A sccb driver for the camera.
+ */
+
+#ifndef SCCB_H
+#define SCCB_H
+
+/**
+ * \brief Initializes the sccb for the camera
+ */
+void sccbInit(void);
+
+#endif
