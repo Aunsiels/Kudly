f8ceff73548a2c9bd0ffa2cd7e03bb7ea76e5871
Dimitri Tuaz <dimitri.tuaz-torchon@telecom-paristech.fr>
2015-04-04 23:38:12 +0200

    WIP

--- soft/src/main.c
+++ soft/src/main.c
@@ -6,6 +6,14 @@ int main(void) {
   halInit();
   chSysInit();
 
+  palSetPadMode(GPIOA,0,PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOA,1,PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOA,2,PAL_MODE_OUTPUT_PUSHPULL);
+
+  palSetPad(GPIOA,0);
+  palSetPad(GPIOA,1);
+  palSetPad(GPIOA,2);
+
   while (TRUE) {
     chThdSleepMilliseconds(500);
   }
