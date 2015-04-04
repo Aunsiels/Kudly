a7eeff3c975983a6a52e7a9776c9df75e485d356
Julien Romero <romerojulien34@gmail.com>
2015-04-04 16:54:12 +0200

    send byte

--- soft/src/sccb.c
+++ soft/src/sccb.c
@@ -80,12 +80,65 @@ void sccbNA(){
     palSetPad(GPIOC, SIO_D);
     chThdSleepMicroseconds(DELAY);
 
+    /* TIC */
     palSetPad(GPIOC, SIO_C);
     chThdSleepMicroseconds(DELAY);
 
+    /* TAC */
     palClearPad(GPIOC, SIO_C);
     chThdSleepMicroseconds(DELAY);
 
+    /* 
+     * When there is an Acknoledgement, it will be the end of the transmission
+     * because we acknowledge only the there is a read answer 
+     */
     palClearPad(GPIOC, SIO_D);
     chThdSleepMicroseconds(DELAY);
+}
+
+/*
+ * Send a byte to the camera
+ */
+int sccbSendByte(uint8_t data){
+    /* Not initialized yet */
+    if(state != SCCB_READY) return;
+    
+    int i, success;
+    /* We send all the bits, beginning the MSB*/
+    for(i = 0; i < 8; ++i){
+        if((data << i) & 0x80)
+            palSetPad(GPIOC, SIO_D);
+        else
+            palClearPad(GPIOC, SIO_D);
+        chThdSleepMicroseconds(DELAY);
+        
+        /* TIC */
+        palSetPad(GPIOC, SIO_C);
+        chThdSleepMicroseconds(DELAY);
+
+        /* TAC */
+        palClearPad(GPIOC, SIO_C);
+        chThdSleepMicroseconds(DELAY);
+    }
+
+    /* Now we check if the camera received something */
+    /* We get ready to ready what is on SIO_D */
+    palSetPadMode(GPIOC, SIO_D, PAL_MODE_INPUT);
+    chThdSleepMicroseconds(DELAY);
+
+    /* TIC */
+    palSetPad(GPIOC, SIO_C);
+    chThdSleepMicroseconds(DELAY);
+
+    /* We read the acknowledgement */
+    success = 1 - palReadPad(GPIOC, SIO_D);    
+
+    /* TAC */
+    palClearPad(GPIOC, SIO_C);
+    chThdSleepMicroseconds(DELAY);
+
+    /* We put the output mode again */
+    palSetPadMode(GPIOC, SIO_D, PAL_MODE_OUTPUT_OPENDRAIN);
+
+    return success;
 }
