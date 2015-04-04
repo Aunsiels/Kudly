7052c016cd643a3263e0e218357b0bc9c21c9f76
Julien Romero <romerojulien34@gmail.com>
2015-04-04 16:03:50 +0200

    init function

--- soft/src/sccb.c
+++ soft/src/sccb.c
@@ -1,4 +1,27 @@
 #include "hal.h"
 #include "ch.h"
+#include "sccb.h"
+
+#define DELAY
+#define SCCB_UNINIT 0
+#define SCCB_READY  1
+#define SIO_C 9
+#define SIO_D 8
+
+static int state = 0;
+
+/*
+ * Initialize the sccb
+ */
+void sccbInit(){
+    /* Open drain mode */
+    palSetPadMode(GPIOC, SIO_D, PAL_MODE_OUTPUT_OPENDRAIN);
+    palSetPadMode(GPIOC, SIO_C, PAL_MODE_OUTPUT_OPENDRAIN);
+    /* Initializes the value of the pins */
+    palSetPad(GPIOC, SIO_C);
+    palSetPad(GPIOC, SIO_D);
+    /* Ready to be used */
+    state = SCCB_READY;
+}
 
 
