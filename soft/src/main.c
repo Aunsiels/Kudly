80c0141d5116c7a666edfc9151add947ef1eef5c
Marc Beillevaire <marc.beillevaire@gmail.com>
2015-04-05 14:07:31 +0200

    WIP - debugging leds

--- soft/src/main.c
+++ soft/src/main.c
@@ -1,10 +1,14 @@
 #include "ch.h"
 #include "hal.h"
 
+#include "led.h"
+
 int main(void) {
 
   halInit();
   chSysInit();
+
+  ledSetColorRGB(0,255, 0, 0);
 
   while (TRUE) {
     chThdSleepMilliseconds(500);
