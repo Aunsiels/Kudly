5cfda7c36677f59a821c912eb99fb3ba032a7c6c
Julien Romero <romerojulien34@gmail.com>
2015-04-07 01:36:44 +0200

    stream page

--- soft/server/app/controllers/Application.scala
+++ soft/server/app/controllers/Application.scala
@@ -4,6 +4,11 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import java.io.File
+import java.io.FileInputStream
+import play.api.libs.iteratee._
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 object Application extends Controller {
 
@@ -19,7 +24,20 @@ object Application extends Controller {
          }
     }
 
+    /*
+     * Send a simple test for leds
+     */
     def pwm = Action {
         Ok("<led>\n<pwm_set n=\"1\" r=\"255\" g=\"125\" b=\"3\"/>\n</led>")
+    }
+
+    def stream = Action {
+        val file = new File("public/0233.ogg")
+        val data = new FileInputStream(file)
+        val dataContent: Enumerator[Array[Byte]] = Enumerator.fromStream(data)
+            
+        Ok.chunked(dataContent).
+            withHeaders( (CONTENT_TYPE, "audio/ogg"),
+                         (CACHE_CONTROL, "no-cache"))
     }
 }
