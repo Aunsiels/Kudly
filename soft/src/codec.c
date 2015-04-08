48f57fa20fd6cbd8f1a5f927d47d3e1b11a55a8b
Antoine Rousseau <antoine.rousseau9@gmail.com>
2015-04-08 10:51:51 +0200

    WIP

--- soft/src/codec.c
+++ soft/src/codec.c
@@ -163,7 +163,7 @@ void codecLowPower(){
 static uint8_t musicBuffer[32];
 
 void codecPlayMusic(char * name){
-  static FIL fil;
+  FIL fil;
   UINT *bytesNumber=0;
   int cptEndFill=0;
   int cptReset=0;
@@ -196,45 +196,45 @@ void codecPlayMusic(char * name){
     }
   }
 }
-//static uint16_t test[32000];
+static uint16_t test[32000];
 
 void writeSoundFile(char * name){
 
-  uint16_t data;
+  //uint16_t data;
   static FIL fil;
-  UINT bw;
+  //UINT bw;
   uint16_t endFillByte;
 
   f_open(&fil,name,FA_WRITE | FA_OPEN_ALWAYS);
 
-
+  /*
   while(1){
     data = readRegister(SCI_HDAT0);
     f_write(&fil,&data,2,&bw);
     if(readRegister(SCI_HDAT1) == 0)
       break;
-      }
+      }*/
 
-  /*int i;
+  int i;
 
   for(i = 0 ; i < 32000 ; i++){
     test[i] = readRegister(SCI_HDAT0);
     if(readRegister(SCI_HDAT1) == 0)
       break;
-      }*/
+  }
 
   endFillByte = readRam(PAR_END_FILL_BYTE);
 
   /* If it's odd lenght, endFillByte should be added */
   if(endFillByte & (1 << 15))
-    f_write(&fil,(uint8_t *)&endFillByte,1,&bw);
-  //test[31999] = (uint8_t)endFillByte;
+    //f_write(&fil,(uint8_t *)&endFillByte,1,&bw);
+    test[31999] = (uint8_t)endFillByte;
 
   f_close(&fil);
   writeRam(PAR_END_FILL_BYTE,0);
 
-  /*for(i = 0;i<32000;i++)
-    writeSerial("%u",test[i]);*/
+  for(i = 0;i<32000;i++)
+    writeSerial("%u",test[i]);
 
 }
 
@@ -247,7 +247,7 @@ void codecEncodeSound(int duration){
   /* Maximum gain amplification at x4 */
   writeRegister(SCI_AICTRL2,4096);
   /* Set in mono mode, and in format OGG Vorbis */
-  writeRegister(SCI_AICTRL3,4|(5 << 4));
+  writeRegister(SCI_AICTRL3,4|(1 << 4));
   /* Set quality mode to 5 */
   writeRegister(SCI_WRAMADDR,0x5);
 
@@ -255,7 +255,7 @@ void codecEncodeSound(int duration){
   writeRegister(SCI_MODE,readRegister(SCI_MODE) | SM_ENCODE);
   writeRegister(SCI_AIADDR,0x50);
 
-  writeSoundFile("testa.ogg");
+  writeSoundFile("test1");
 
   /* Collect the data in HDAT0/1 */
   chThdSleepMilliseconds(duration);
@@ -265,10 +265,7 @@ void codecEncodeSound(int duration){
   /* Stop the acquisition */
   writeRegister(SCI_MODE,readRegister(SCI_MODE) | SM_CANCEL);
   /* Wait until the codec exit the encoding mode */
-
-  //palSetPad(GPIOA,0);
-
-  while((readRegister(SCI_MODE) & SM_ENCODE) == 1);
+  //while((readRegister(SCI_MODE) & SM_ENCODE) == 1);
 
   codecReset();
 
