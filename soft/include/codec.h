9328ffaca3872fed29671e69fb3a9b4cd05cfa04
Antoine Rousseau <antoine.rousseau9@gmail.com>
2015-04-09 23:16:07 +0200

    Adding of Doxygen commentaries

--- soft/include/codec.h
+++ soft/include/codec.h
@@ -1,18 +1,105 @@
+/**
+ * \file codec.h
+ * \brief Control audio codec
+ * \author KudlyProject
+ * \version 0.1
+ * \date 09/04/2015
+ *
+ * Manages the ADC hand sensors conversions.
+ *
+ */
+
 #ifndef CODEC_H
 #define CODEC_H
 
+/**
+ *
+ * \brief Initializes audio codec
+ *
+ * This function configures pad modes to enable SPI communication  
+ */
+
 void codecInit(void);
+
+/**
+ *
+ * \brief Initializes codec registers 
+ *
+ * This function configures SCI_CLOCKF, SCI_AUDATA and SCI_VOL registers in order to consume less energy   
+ */
+
 void codecLowPower(void);
 
+/**
+ *
+ * \brief Writes 16 bits data in the codec RAM
+ *
+ * This function writes in the registers SCI_WRAMADDR and SCI_WRAM in order to send 16 bits data   
+ */
+
 void writeRam(uint16_t,uint16_t);
+
+/**
+ *
+ * \brief Writes 32 bits data in the codec RAM
+ *
+ * This function writes once in the register SCI_WRAMADDR and twice in the register SCI_WRAM in order to send 32 bits data   
+ */
+
 void writeRam32(uint16_t,uint32_t);
+
+/**
+ *
+ * \brief Reads 16 bits data in the codec RAM
+ *
+ * This function writes in the register SCI_WRAMADDR, the address of our data and then returns the contain of the register on 16 bits   
+ */
+
 uint16_t readRam(uint16_t);
+
+/**
+ *
+ * \brief Reads 32 bits data in the codec RAM
+ *
+ * This function writes in the register SCI_WRAMADDR, the address and the address +1 of our data and then returns the contain of the registers on 32 bits  
+ */
+
 uint32_t readRam32(uint16_t);
 
+/**
+ *
+ * \brief Displays a music file
+ *
+ * This function follows the sending protocol wrote in the VS1063 datasheet. It is also able to send a file from the SDcard 
+ */
+
 void codecPlayMusic(char *);
+
+/**
+ *
+ * \brief Encodes a sound from the microphone
+ *
+ * This function needs a name file and a time of recording to be enabled. It writes in a file on the SDcard a sound with an .ogg format  
+ */
+
 void codecEncodeSound(char *,int);
 
+/**
+ *
+ * \brief Shell command for displaying a sound
+ *
+ * This function enables the codecPlayMusic function in a shell command  
+ */
+
 void cmdPlay(BaseSequentialStream *, int, char *[]);
+
+/**
+ *
+ * \brief Shell command for encoding a sound
+ *
+ * This function enables the Encode function in a shell command  
+ */
+
 void cmdEncode(BaseSequentialStream *, int, char *[]);
 
 #endif /* CODEC_H */
