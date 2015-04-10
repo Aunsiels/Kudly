8ce96c7c5b529c46aa88eee2d4abebaa58fc7576
Antoine Rousseau <antoine.rousseau9@gmail.com>
2015-04-10 09:27:26 +0200

    Little modifications for respecting our style guide

--- soft/src/wifi.c
+++ soft/src/wifi.c
@@ -61,61 +61,61 @@ static msg_t usartRead_thd(void * args) {
     enum state state = WAIT_FEATURE;
 
     while(1) {
-      if(chMBFetch(&mb, (msg_t *)&c, TIME_INFINITE) == RDY_OK) {
-	writeSerial("%c", c);
+        if(chMBFetch(&mb, (msg_t *)&c, TIME_INFINITE) == RDY_OK) {
+            writeSerial("%c", c);
 	
-	/* Wait for feature frame beginning */
-	if (state == WAIT_FEATURE){
-	  if (c == '<') {
-	    parse_feature = 0;
-	    state = WRITE_FEATURE;
-	    continue;
-	  }
-	}
+            /* Wait for feature frame beginning */
+            if (state == WAIT_FEATURE){
+                if (c == '<') {
+                    parse_feature = 0;
+                    state = WRITE_FEATURE;
+                    continue;
+                }
+            }
 	
-	/* Write feature frame in feture buffer */
-	if (state == WRITE_FEATURE){
-	  if (c == '>'){
-	    state = WAIT_FUNCTION;
-	    feature[parse_feature] = '\0';
-	    continue;
-	  }
-	    feature[parse_feature] = c;
-	    parse_feature++;
-	}
+            /* Write feature frame in feture buffer */
+            if (state == WRITE_FEATURE){
+                if (c == '>'){
+                    state = WAIT_FUNCTION;
+                    feature[parse_feature] = '\0';
+                    continue;
+                }
+                feature[parse_feature] = c;
+                parse_feature++;
+            }
 	
-	/* Wait for function frame beginning */
-	if (state == WAIT_FUNCTION){
-	  if (c == '<'){
-	    state = WRITE_FUNCTION;
-	    parse_function = 0;
-	    continue;
-	  } 
-	}
+            /* Wait for function frame beginning */
+            if (state == WAIT_FUNCTION){
+                if (c == '<'){
+                    state = WRITE_FUNCTION;
+                    parse_function = 0;
+                    continue;
+                } 
+            }
 
-	/* Write function in frame buffer */
-	if (state == WRITE_FUNCTION){
-	  if (c == '>'){
-	    state = END_FEATURE;
-	    function[parse_function-1] = '\0';
-	    /* Function and feature are ready : send event broadcast */
-	    chSysLock();
-	    chEvtBroadcastI(&eventWifiSrc);
-	    chSysUnlock();
-	    continue;
-	  }
-	    function[parse_function]=c;
-	    parse_function++;
-	}
+            /* Write function in frame buffer */
+            if (state == WRITE_FUNCTION){
+                if (c == '>'){
+                    state = END_FEATURE;
+                    function[parse_function-1] = '\0';
+                    /* Function and feature are ready : send event broadcast */
+                    chSysLock();
+                    chEvtBroadcastI(&eventWifiSrc);
+                    chSysUnlock();
+                    continue;
+                }
+                function[parse_function]=c;
+                parse_function++;
+            }
 	
-	/* Wait for feature frame end */
-	if (state == END_FEATURE){
-	  if (c == '>'){
-	    state = WAIT_FEATURE;
-	    continue;
-	  }
-	}
-      }
+            /* Wait for feature frame end */
+            if (state == END_FEATURE){
+                if (c == '>'){
+                    state = WAIT_FEATURE;
+                    continue;
+                }
+            }
+        }
     } 
     return 0;
 }
@@ -161,12 +161,12 @@ void wifiReadByUsart(void) {
     static WORKING_AREA(usartReadInMB_wa, 128);
     
     chThdCreateStatic(
-	usartReadInMB_wa, sizeof(usartReadInMB_wa),
-	NORMALPRIO, usartReadInMB_thd, NULL);
+        usartReadInMB_wa, sizeof(usartReadInMB_wa),
+        NORMALPRIO, usartReadInMB_thd, NULL);
     
     chThdCreateStatic(
-	usartRead_wa, sizeof(usartRead_wa),
-	NORMALPRIO, usartRead_thd, NULL);
+        usartRead_wa, sizeof(usartRead_wa),
+        NORMALPRIO, usartRead_thd, NULL);
 }
 
 /* Command shell to speak with wifi module in command mode */
@@ -174,60 +174,60 @@ void cmdWifi(BaseSequentialStream *chp, int argc, char *argv[]){
     (void)chp;
     int i;
     for(i = 0; i < argc; i++){
-	wifiWriteByUsart(argv[i], strlen(argv[i]));
-    wifiWriteByUsart(space, sizeof(space));
-  }
-  wifiWriteByUsart(crlf, sizeof(crlf));
+        wifiWriteByUsart(argv[i], strlen(argv[i]));
+        wifiWriteByUsart(space, sizeof(space));
+    }
+    wifiWriteByUsart(crlf, sizeof(crlf));
 }
 
 /* Thread waits an wifi event and parse feature and function to launch the rigth function */
 static msg_t wifiCommands_thd(void * args) {
-  (void)args;  
-  EventListener eventWifiLst;
-  chEvtRegisterMask(&eventWifiSrc, &eventWifiLst, 1);
-  char* ptr;  
-  while(1) {
-    chEvtWaitOne(1);
-    if( NULL != strstr(feature,"led")){
-      if ( NULL != strstr(function,"rgb_set")){
-	int n = strtol(strstr(function,"n=\"") +3,&ptr,10);
-	int r = strtol(strstr(function,"r=\"") +3,&ptr,10);
-	int g = strtol(strstr(function,"g=\"") +3,&ptr,10);
-	int b = strtol(strstr(function,"b=\"") +3,&ptr,10);
-	ledSetColorRGB(n, r, g, b);
-      continue;
-      }
+    (void)args;  
+    EventListener eventWifiLst;
+    chEvtRegisterMask(&eventWifiSrc, &eventWifiLst, 1);
+    char* ptr;  
+    while(1) {
+        chEvtWaitOne(1);
+        if( NULL != strstr(feature,"led")){
+            if ( NULL != strstr(function,"rgb_set")){
+                int n = strtol(strstr(function,"n=\"") +3,&ptr,10);
+                int r = strtol(strstr(function,"r=\"") +3,&ptr,10);
+                int g = strtol(strstr(function,"g=\"") +3,&ptr,10);
+                int b = strtol(strstr(function,"b=\"") +3,&ptr,10);
+                ledSetColorRGB(n, r, g, b);
+                continue;
+            }
       
-      if ( NULL != strstr(function,"hsv_set")){	
-	int n = strtol(strstr(function,"n=\"") +3,&ptr,10);
-	int h = strtol(strstr(function,"h=\"") +3,&ptr,10);
-	int s = strtol(strstr(function,"s=\"") +3,&ptr,10);
-	int v = strtol(strstr(function,"v=\"") +3,&ptr,10);
-	ledSetColorHSV(n, h, s, v);
-      continue;
-      }
+            if ( NULL != strstr(function,"hsv_set")){	
+                int n = strtol(strstr(function,"n=\"") +3,&ptr,10);
+                int h = strtol(strstr(function,"h=\"") +3,&ptr,10);
+                int s = strtol(strstr(function,"s=\"") +3,&ptr,10);
+                int v = strtol(strstr(function,"v=\"") +3,&ptr,10);
+                ledSetColorHSV(n, h, s, v);
+                continue;
+            }
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 
 
 /* Launches thread that wait wifi event */
 void wifiCommands(void) {
-  static WORKING_AREA(wifiCommands_wa, 128);
-  chThdCreateStatic(
-		    wifiCommands_wa, sizeof(wifiCommands_wa),
-		    NORMALPRIO, wifiCommands_thd, NULL);
+    static WORKING_AREA(wifiCommands_wa, 128);
+    chThdCreateStatic(
+        wifiCommands_wa, sizeof(wifiCommands_wa),
+        NORMALPRIO, wifiCommands_thd, NULL);
 }
 
 /* Command shell to send http request and read the stream */
 void cmdWifiTest
 (BaseSequentialStream *chp, int argc, char *argv[]) {
-  (void)chp;
-  (void)argc;
-  (void)argv;
-  wifiWriteByUsart(http_get, sizeof(http_get));
-  wifiWriteByUsart(stream_read, sizeof(stream_read));
-  wifiWriteByUsart(stream_close, sizeof(stream_close));
+    (void)chp;
+    (void)argc;
+    (void)argv;
+    wifiWriteByUsart(http_get, sizeof(http_get));
+    wifiWriteByUsart(stream_read, sizeof(stream_read));
+    wifiWriteByUsart(stream_close, sizeof(stream_close));
   
 }
