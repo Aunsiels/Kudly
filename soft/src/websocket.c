507dd33df4a79232c10fd238a40ed18323800873
Marc Beillevaire <marc.beillevaire@gmail.com>
2015-04-11 21:09:41 +0200

    WIP

--- soft/src/websocket.c
+++ soft/src/websocket.c
@@ -6,29 +6,33 @@
 #include "chprintf.h"
 #include "wifi.h"
 
-static char ECHO_1[] = "http_get -o kudly.herokuapp.com/echo\n\r";
-static char ECHO_2[] = "had 0 Upgrade websocket\n\r";
-static char ECHO_3[] = "had 0 Connection Upgrade\n\r";
-static char ECHO_4[] = "had 0 Sec-WebSocket-Key x3JJHMbDL1EzLkh9GBhXDw==\n\r";
-static char ECHO_5[] = "had 0 Sec-WebSocket-Protocol chat\n\r";
-static char ECHO_6[] = "had 0 Sec-WebSocket-Version 13\n\r";
-static char ECHO_7[] = "had 0 Origin http://example.com\n\r";
-static char ECHO_8[] = "hre 0\n\r";
+static char tcpc[] = "tcpc kudly.herokuapp.com 80\r\n";
+static char streamWrite[] = "write 0 284\r\n";
+
+static char webSocketClient1[] =
+"GET /echo HTTP/1.1\r\n\
+Host: kudly.herokuapp.com\r\n\
+Pragma: no-cache\r\n\
+Cache-Control: no-cache\r\n\
+Upgrade: websocket\r\n";
+
+static char webSocketClient2[] =
+"Connection: Upgrade\r\n\
+Sec-WebSocket-Key: x3JJHRbDL1EzLkh9GBhXDw==\r\n\
+Sec-WebSocket-Protocol: chat, superchat\r\n\
+Sec-WebSocket-Version: 13\r\n\
+Origin: http://kudly.herokuapp.com\r\n\
+\r\n";
 
 /*
  * Initializes a websocket connection
  */
 void websocketInit(void){
     /* Init sequence */
-    sdWrite(&SD3, (uint8_t*) ECHO_1, sizeof(ECHO_1));
-    sdWrite(&SD3, (uint8_t*) ECHO_2, sizeof(ECHO_2));
-    sdWrite(&SD3, (uint8_t*) ECHO_3, sizeof(ECHO_3));
-    sdWrite(&SD3, (uint8_t*) ECHO_4, sizeof(ECHO_4));
-    sdWrite(&SD3, (uint8_t*) ECHO_5, sizeof(ECHO_5));
-    sdWrite(&SD3, (uint8_t*) ECHO_6, sizeof(ECHO_6));
-    sdWrite(&SD3, (uint8_t*) ECHO_7, sizeof(ECHO_7));
-    sdWrite(&SD3, (uint8_t*) ECHO_8, sizeof(ECHO_8));
-    /* Read answer */
+    wifiWriteByUsart(tcpc, sizeof(tcpc));
+    wifiWriteByUsart(streamWrite, sizeof(streamWrite));
+    wifiWriteByUsart(webSocketClient1, sizeof(webSocketClient1));
+    wifiWriteByUsart(webSocketClient2, sizeof(webSocketClient2));
 }
 
 /*
@@ -68,15 +72,7 @@ void cmdWebSoc(BaseSequentialStream* chp, int argc, char * argv[]) {
     (void)argc;
     (void)argv;
 
-    static char streamWrite[] = "stream_write 0 5\r\n";
-    static char specialChars[] = {0x81, 0x01, 0x65};
-
     websocketInit();
 
-    chThdSleepMilliseconds(1000);
-
-    wifiWriteByUsart(streamWrite, sizeof(streamWrite));
-    wifiWriteByUsart(specialChars, sizeof(specialChars));
-    wifiWriteByUsart("\n\r", 2);
 }
 
