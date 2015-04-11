e84c52016c0cca3f4fdb7314e488ba6ac613db47
Dimitri Tuaz <dimitri.tuaz-torchon@telecom-paristech.fr>
2015-04-11 17:12:00 +0200

    Clean Up

--- soft/src/codec.c
+++ soft/src/codec.c
@@ -25,6 +25,18 @@ static WORKING_AREA(waPlayback, 2048);
 static WORKING_AREA(waWaitEncoding, 128);
 volatile int volLevel = 5;
 
+/* Functions used to access registers and ram of the codec */
+static void writeRegister(uint8_t adress, uint16_t command);
+static void writeRam(uint16_t adress, uint16_t data);
+void writeRam32(uint16_t adress, uint32_t data);
+static uint16_t readRegister(uint8_t adress);
+static uint16_t readRam(uint16_t adress);
+uint32_t readRam32(uint16_t adress);
+/* Function used to send data to the codec (no more than 32 bytes) */
+static void sendData(const uint8_t * data, int size);
+/* Function used to load a patch in the codec (called at each software register) */
+static void loadPatch(void);
+
 /* SPI configuration (21MHz, CPHA=0, CPOL=0, MSb first) */
 static const SPIConfig hs_spicfg = {
     NULL,
@@ -33,133 +45,6 @@ static const SPIConfig hs_spicfg = {
     SPI_CR1_BR_1 | SPI_CR1_BR_0
 };
 
-/* Buffer used for construcion of read and write command instructions */
-static uint8_t instruction[4];
-static uint8_t registerContent[4];
-
-/* Write in a register of the codec */
-static void writeRegister(uint8_t adress, uint16_t command){
-    /* Wait until it's possible to write in registers */
-    while((palReadPad(GPIOE,GPIOE_CODEC_DREQ) == 0));
-    
-    COMMAND_MODE;
-    
-    /* Construction of instruction (Write opcode, adress, command) */
-    instruction[0] = 0x02;
-    instruction[1] = adress;
-    instruction[2] = (command >> 8);
-    instruction[3] = command;
-    spiSend(&SPID4,sizeof(instruction),instruction);
-    
-    RESET_MODE;
-}
-
-/* Write a 16 bit data in the ram of the codec */
-void writeRam(uint16_t adress, uint16_t data){
-    writeRegister(SCI_WRAMADDR,adress);
-    writeRegister(SCI_WRAM,data);
-}
-
-/* Write a 32 bit data in the ram of the codec */
-void writeRam32(uint16_t adress, uint32_t data){
-    writeRegister(SCI_WRAMADDR,adress);
-    writeRegister(SCI_WRAM,(uint16_t) data);
-    writeRegister(SCI_WRAM,(uint16_t) (data >> 16));
-}
-
-/* Read in  a register of a codec */
-static uint16_t readRegister(uint8_t adress){
-    /* Wait until it's possible to read from SCI */
-    while((palReadPad(GPIOE,GPIOE_CODEC_DREQ) == 0));
-
-    COMMAND_MODE;
-
-    /* Construction of instruction (Read opcode, adress) */
-    instruction[0] = 0x03;
-    instruction[1] = adress;
-   
-    spiExchange(&SPID4,sizeof(instruction),instruction,registerContent);
-    
-    RESET_MODE;
-    
-    /* Return only the 2 last bytes (data from the register) */
-    return ((registerContent[2]<<8) + registerContent[3]);
-}
-
-/* Read a 16 bit data from the ram of the codec */
-uint16_t readRam(uint16_t adress){
-    writeRegister(SCI_WRAMADDR,adress);
-    return readRegister(SCI_WRAM);
-}
-
-/* Read a 32 bit data from the ram of the codec */
-uint32_t readRam32(uint16_t adress){
-    uint16_t lsb,msb;
-    writeRegister(SCI_WRAMADDR,adress+1);
-    msb = readRegister(SCI_WRAM);
-    writeRegister(SCI_WRAMADDR,adress);
-    lsb = readRegister(SCI_WRAM);
-    return (lsb |((uint32_t)msb << 16));
-}
-
-/* Function to send data (SDI), maximum of 32 bytes */
-void sendData(const uint8_t * data, int size){
-    int i;
-
-    /* Wait until it's possible to send data */
-    while((palReadPad(GPIOE,GPIOE_CODEC_DREQ) == 0));
-
-    DATA_MODE;
-
-    for(i = 0 ; i < size ; i++)
-	spiSend(&SPID4,1,data++);
-
-    RESET_MODE;
-}
-
-static void loadPatch(void) {
-    int i;
-    for (i=0;i<CODE_SIZE;i++) {
-        writeRegister(atab[i], dtab[i]);
-    }
-}
-
-
-void codecReset(void){
-    /* Start of SPI bus */ 
-    spiStart(&SPID4, &hs_spicfg);
-    spiSelect(&SPID4);
-
-    RESET_MODE;
-
-    /* Software reset of the codec */
-    writeRegister(SCI_MODE,SM_RESET);
-    /* Wait until reset is complete */
-    while(palReadPad(GPIOE,GPIOE_CODEC_DREQ) == 0);
-
-    /* Load the patch of the codec */
-    loadPatch();
-
-    /* Use native SPI modes and use both XDCS ans XCS for chip select */
-    writeRegister(SCI_MODE,SM_SDINEW);
-    /* Set Clock settings : x4.5 multiplier (+ x1 when needed, to encode in Ogg Vorbis)  */
-    writeRegister(SCI_CLOCKF,SC_MULT_53_45X|SC_ADD_53_10X);
-    /* Set encoding samplerate to 16000Hz, in mono mode */
-    writeRegister(SCI_AUDATA,0x3E80);
-    /* Both left and right volumes are at middle (5 over 10) */
-    codecVolume(volLevel);
-}
-
-void codecLowPower(){
-    /* Set clock settings : x1.0 to disable the PLL and save power */
-    writeRegister(SCI_CLOCKF,0x0000);
-    /* Reduce the samplerate, the VSDSP core will just wait for an interrupt, thus saving power */
-    writeRegister(SCI_AUDATA,0x0010);
-    /* Set the attenuation to his maximum */
-    codecVolume(0);
-    /* Stop the SPI bus */
-    spiStop(&SPID4);
-}
 
 static uint8_t playBuf[FILE_BUFFER_SIZE];
 static FIL readFp;
@@ -211,6 +96,10 @@ static msg_t threadPlayback(void *arg){
 		}
 		
 	    default:
+		break;
+	    }
+	    if(control == 'q'){
+		control = 0;
 		break;
 	    }
 	    control = 0;
@@ -361,24 +250,6 @@ static msg_t threadEncode(void *arg){
     return 0;
 }
 
-void codecInit(){
-    /* Change the mode of the pins used for the codec and his SPI bus */
-    palSetPadMode(GPIOE,GPIOE_SPI4_XDCS,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
-    palSetPadMode(GPIOE,GPIOE_SPI4_XCS,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
-    palSetPadMode(GPIOE,GPIOE_CODEC_DREQ,PAL_MODE_INPUT_PULLUP | PAL_STM32_OSPEED_HIGHEST);
-    palSetPadMode(GPIOE,GPIOE_SPI4_SCK,PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);
-    palSetPadMode(GPIOE,GPIOE_SPI4_MISO,PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);
-    palSetPadMode(GPIOE,GPIOE_SPI4_MOSI,PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);
-    
-    spiAcquireBus(&SPID4);
-
-    codecReset();
-    
-    /* Create the threads to perform playback and recording (they are waiting on en eventlistener) */
-    chThdCreateStatic(waPlayback, sizeof(waPlayback),NORMALPRIO, threadPlayback,NULL);
-    chThdCreateStatic(waEncode, sizeof(waEncode),NORMALPRIO, threadEncode,NULL);
-}
-
 void codecVolume(int volume) {
     
     switch(volume){
@@ -462,4 +333,153 @@ void cmdControl(BaseSequentialStream *chp, int argc, char *argv[]) {
         
     return;
 }
+
+
+void codecInit(){
+    /* Change the mode of the pins used for the codec and his SPI bus */
+    palSetPadMode(GPIOE,GPIOE_SPI4_XDCS,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(GPIOE,GPIOE_SPI4_XCS,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(GPIOE,GPIOE_CODEC_DREQ,PAL_MODE_INPUT_PULLUP | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(GPIOE,GPIOE_SPI4_SCK,PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(GPIOE,GPIOE_SPI4_MISO,PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(GPIOE,GPIOE_SPI4_MOSI,PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);
+    
+    spiAcquireBus(&SPID4);
+
+    codecReset();
+    
+    /* Create the threads to perform playback and recording (they are waiting on en eventlistener) */
+    chThdCreateStatic(waPlayback, sizeof(waPlayback),NORMALPRIO, threadPlayback,NULL);
+    chThdCreateStatic(waEncode, sizeof(waEncode),NORMALPRIO, threadEncode,NULL);
+}
+
+void codecReset(void){
+    /* Start of SPI bus */ 
+    spiStart(&SPID4, &hs_spicfg);
+    spiSelect(&SPID4);
+
+    RESET_MODE;
+
+    /* Software reset of the codec */
+    writeRegister(SCI_MODE,SM_RESET);
+    /* Wait until reset is complete */
+    while(palReadPad(GPIOE,GPIOE_CODEC_DREQ) == 0);
+
+    /* Load the patch of the codec */
+    loadPatch();
+
+    /* Use native SPI modes and use both XDCS ans XCS for chip select */
+    writeRegister(SCI_MODE,SM_SDINEW);
+    /* Set Clock settings : x4.5 multiplier (+ x1 when needed, to encode in Ogg Vorbis)  */
+    writeRegister(SCI_CLOCKF,SC_MULT_53_45X|SC_ADD_53_10X);
+    /* Set encoding samplerate to 16000Hz, in mono mode */
+    writeRegister(SCI_AUDATA,0x3E80);
+    /* Both left and right volumes are at middle (5 over 10) */
+    codecVolume(volLevel);
+}
+
+void codecLowPower(void){
+    /* Set clock settings : x1.0 to disable the PLL and save power */
+    writeRegister(SCI_CLOCKF,0x0000);
+    /* Reduce the samplerate, the VSDSP core will just wait for an interrupt, thus saving power */
+    writeRegister(SCI_AUDATA,0x0010);
+    /* Set the attenuation to his maximum */
+    codecVolume(0);
+    /* Stop the SPI bus */
+    spiStop(&SPID4);
+}
+
+
+/* Buffer used for construcion of read and write command instructions */
+static uint8_t instruction[4];
+static uint8_t registerContent[4];
+
+/* Write in a register of the codec */
+static void writeRegister(uint8_t adress, uint16_t command){
+    /* Wait until it's possible to write in registers */
+    while((palReadPad(GPIOE,GPIOE_CODEC_DREQ) == 0));
+    
+    COMMAND_MODE;
+    
+    /* Construction of instruction (Write opcode, adress, command) */
+    instruction[0] = 0x02;
+    instruction[1] = adress;
+    instruction[2] = (command >> 8);
+    instruction[3] = command;
+    spiSend(&SPID4,sizeof(instruction),instruction);
+    
+    RESET_MODE;
+}
+
+/* Write a 16 bit data in the ram of the codec */
+static void writeRam(uint16_t adress, uint16_t data){
+    writeRegister(SCI_WRAMADDR,adress);
+    writeRegister(SCI_WRAM,data);
+}
+
+/* Write a 32 bit data in the ram of the codec */
+void writeRam32(uint16_t adress, uint32_t data){
+    writeRegister(SCI_WRAMADDR,adress);
+    writeRegister(SCI_WRAM,(uint16_t) data);
+    writeRegister(SCI_WRAM,(uint16_t) (data >> 16));
+}
+
+/* Read in  a register of a codec */
+static uint16_t readRegister(uint8_t adress){
+    /* Wait until it's possible to read from SCI */
+    while((palReadPad(GPIOE,GPIOE_CODEC_DREQ) == 0));
+
+    COMMAND_MODE;
+
+    /* Construction of instruction (Read opcode, adress) */
+    instruction[0] = 0x03;
+    instruction[1] = adress;
+   
+    spiExchange(&SPID4,sizeof(instruction),instruction,registerContent);
+    
+    RESET_MODE;
+    
+    /* Return only the 2 last bytes (data from the register) */
+    return ((registerContent[2]<<8) + registerContent[3]);
+}
+
+/* Read a 16 bit data from the ram of the codec */
+static uint16_t readRam(uint16_t adress){
+    writeRegister(SCI_WRAMADDR,adress);
+    return readRegister(SCI_WRAM);
+}
+
+/* Read a 32 bit data from the ram of the codec */
+uint32_t readRam32(uint16_t adress){
+    uint16_t lsb,msb;
+    writeRegister(SCI_WRAMADDR,adress+1);
+    msb = readRegister(SCI_WRAM);
+    writeRegister(SCI_WRAMADDR,adress);
+    lsb = readRegister(SCI_WRAM);
+    return (lsb |((uint32_t)msb << 16));
+}
+
+/* Function to send data (SDI), maximum of 32 bytes */
+static void sendData(const uint8_t * data, int size){
+    int i;
+
+    /* Wait until it's possible to send data */
+    while((palReadPad(GPIOE,GPIOE_CODEC_DREQ) == 0));
+
+    DATA_MODE;
+
+    for(i = 0 ; i < size ; i++)
+	spiSend(&SPID4,1,data++);
+
+    RESET_MODE;
+}
+
+/* Function to load the patch in the codec */
+static void loadPatch(void){
+    int i;
+    for (i=0;i<CODE_SIZE;i++) {
+        writeRegister(atab[i], dtab[i]);
+    }
+}
+
 
