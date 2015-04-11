e2cc053faff1cd6718caf6c712884c392d226e8b
Julien Romero <romerojulien34@gmail.com>
2015-04-11 15:02:47 +0200

    send ws function pseudo code

--- soft/src/websocket.c
+++ soft/src/websocket.c
@@ -2,6 +2,7 @@
 
 #include "hal.h"
 #include "ch.h"
+#include "string.h"
 
 #define ECH0_1 "http_get -o kudly.herokuapp.com/echo"
 #define ECHO_2 "had 0 Upgrade websocket"
@@ -27,3 +28,36 @@ void websocketInit(void){
     sdWrite(&SD3, (uint8_t*) ECHO_8, sizeof(ECHO_8));
     /* Read answer */
 }
+
+/*
+ * Encode a string to send
+ */
+void websocketEncode(char * str){
+
+    // SEND 129
+    int length = strlen(str);
+
+    if (length < 126) {
+        //send (char) length
+    } else if (length < 65536) {
+        //send 126
+        //send (char) ((length >> 8) & 255)
+        //send (char) (length & 255)
+    } else {
+        //send 127
+        //send (char) (length >> 56 & 255)
+        //send (char) (length >> 48 & 255)
+        //send (char) (length >> 40 & 255)
+        //send (char) (length >> 32 & 255)
+        //send (char) (length >> 24 & 255)
+        //send (char) (length >> 16 & 255)
+        //send (char) (length >>  8 & 255)
+        //send (char) (length       & 255)
+    }
+
+    int i;
+    for(i = 0; i < length; ++i){
+        //send str[i]
+    }
+}
+
