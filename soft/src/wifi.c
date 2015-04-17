87f03f4cb0ccf3d137ed198684142701774796f4
Damien Auffret <damien.auffret@telecom-paristech.fr>
2015-04-17 23:03:11 +0200

    Change speed of usart

--- soft/src/wifi.c
+++ soft/src/wifi.c
@@ -36,27 +36,13 @@ static char cfg_printLevel0[] = "set system.print_level 0\r\n";
 static char cfg_promptOff[] = "set system.cmd.prompt_enabled 0\r\n";
 static char cfg_headersOn[] = "set system.cmd.header_enabled 1\r\n";
 
-/* Wifi command to set it in high speed */
-static char uart_baud [] = "set ua b 0 921600\r\n";
-static char uart_flow [] = "set ua f 0 on\r\n";
-static char reboot [] = "reboot\r\n";
-
 /* Serial driver that uses usart3 */
-static SerialConfig uartCfgHigh =
+static SerialConfig uartCfg =
 {
     921600,
     0,
     0,
     USART_CR3_CTSE | USART_CR3_RTSE
-};
-
-/* Serial driver that uses usart3 */
-static SerialConfig uartCfgLow =
-{
-    115200,
-    0,
-    0,
-    0
 };
 
 /* Thread that reads wifi data and puts it on Mailbox */
@@ -76,11 +62,6 @@ void wifiWriteByUsart(char * message, int length){
     sdWrite(&SD3, (uint8_t*)message, length);
     chEvtWaitOne(1);
     chEvtUnregister(&srcEndToReadUsart, &lstEndToReadUsart);
-}
-
-/* Sends data by wifi */
-void wifiWriteByUsartNoWait(char * message, int length){
-    sdWrite(&SD3, (uint8_t*)message, length);
 }
 
 /* Same as above but don't want to wait for the response */
@@ -113,51 +94,27 @@ void cmdWifi(BaseSequentialStream *chp, int argc, char *argv[]){
 /* Initialization of wifi network */
 void wifiInitByUsart(void) {
  
-
     palSetPadMode (GPIOD,GPIOD_WIFI_UART_TX, PAL_MODE_ALTERNATE(7));
     palSetPadMode (GPIOD,GPIOD_WIFI_UART_RX, PAL_MODE_ALTERNATE(7));
     palSetPadMode (GPIOD,GPIOD_WIFI_UART_CTS, PAL_MODE_ALTERNATE(7));
     palSetPadMode (GPIOD,GPIOD_WIFI_UART_RTS, PAL_MODE_ALTERNATE(7));
 
-    chThdSleepMilliseconds(10000);
-
-    sdStart(&SD3, &uartCfgLow);
-    writeSerial("1\r\n");
-
-    wifiWriteByUsartNoWait(cfg_echoOff, sizeof(cfg_echoOff));
-    wifiWriteByUsartNoWait(cfg_printLevel0, sizeof(cfg_printLevel0));
-    wifiWriteByUsartNoWait(cfg_headersOn, sizeof(cfg_headersOn));
-    wifiWriteByUsartNoWait(cfg_promptOff, sizeof(cfg_promptOff));
-    wifiWriteByUsartNoWait(ssid, sizeof(ssid));
-    wifiWriteByUsartNoWait(passkey, sizeof(passkey));
-    wifiWriteByUsartNoWait(uart_baud, sizeof(uart_baud));
-    wifiWriteByUsartNoWait(uart_flow, sizeof(uart_flow));
-    wifiWriteByUsartNoWait(save, sizeof(save));
-    wifiWriteByUsartNoWait(reboot, sizeof(reboot));
-
-    chThdSleepMilliseconds(5000);
-    writeSerial("2\r\n");
-    sdStop(&SD3);
-    writeSerial("3\r\n");
-    sdStart(&SD3, &uartCfgHigh);
-    writeSerial("4\r\n");
+    wifiReadByUsart();
 
     /* Read wifi by usart */
     usartRead();
+  
+    sdStart(&SD3, &uartCfg);
 
-    wifiReadByUsart();
+    wifiWriteByUsart(cfg_echoOff, sizeof(cfg_echoOff));
+    wifiWriteByUsart(cfg_printLevel0, sizeof(cfg_printLevel0));
+    wifiWriteByUsart(cfg_headersOn, sizeof(cfg_headersOn));
+    wifiWriteByUsart(cfg_promptOff, sizeof(cfg_promptOff));
     wifiWriteByUsart(ssid, sizeof(ssid));
     wifiWriteByUsart(passkey, sizeof(passkey));
     wifiWriteByUsart(save, sizeof(save));
-
     wifiWriteByUsart(nup, sizeof(nup));
-    writeSerial("5\r\n");
-      
-    chThdSleepMilliseconds(4000);
+    chThdSleepMilliseconds(5000);
     wifiWriteByUsart(nup, sizeof(nup));
     writeSerial("Wifi ready to use\r\n");
-    /*
-     * Configuring wifi module in machine friendly command mode
-     * cf : http://wiconnect.ack.me/2.1/serial_interface#configuration
-     */
 }
